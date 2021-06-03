796203fc55c3e904dbba22510b62dae8ee13a6c9
SameerKitherMuhamed <85026529+SameerKitherMuhamed@users.noreply.github.com>
2021-06-03 16:36:44 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,15 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int n) {
-  float x,y=1.0;
-  for(int i=0; i<n; i++) {
-    x=1.0*(4*i*i)/(4*i*i-1);
-    y=x*y;
-  }
-  
-  return(2*y);
-}
+float wallis_pi(int);
 
 int main(void) {
   float pi;
@@ -32,6 +24,14 @@ int main(void) {
   }
 }
 
+float wallis_pi(int n){
+  float x,y=1.0;
+  for(int i=1;i<=n;i++){
+    x =1.0*(4*i*i)/(4*i*i-1);
+    y=x*y;
+  }
+return (2*y);
+}
 
 
 
